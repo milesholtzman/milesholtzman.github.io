e860a49ac4531ecd274105dbdc5dd27afc0abeaf
Miles Holtzman <milesholtzman@gmail.com>
2020-08-02 20:26:19 -0400

    added slide and fade prototypes

--- JS/enter.js
+++ JS/enter.js
@@ -1,17 +1,40 @@
 $(document).ready(function(){
-        var blockWidth = $(".block").width();
-        $(".slide-left").click(function(){
+        $(".clickText").click(function(){
+            var browserWidth = $(window).width();
+            $(".homepage-text").addClass("fadeOut")
+            $(".navbar").removeClass("hidden")
             $(".block").animate({
-                width: 0
+                width: browserWidth*0.62
+            }, {
+                duration: 1000
             });
-        });
-        $(".slide-right").click(function(){
-            $(".block").animate({
-                width: blockWidth
-            });
+        ;
         });
     });
 
+// function fadeOut(element) {
+//     $(".homepage-text").addClass("fadeOut");
+// };
+
+// function fadeIn(element) {
+//     $(".navbar").removeClass("hidden")
+// };
+
+// function slide(element) {
+//     var browserWidth = $(window).width();
+//     $(".block").animate({
+//                 width: browserWidth*0.62
+//             });
+// };
+
+// $(document).ready(function(){
+//     $(".clickText").click(function(){
+//         fadeOut();
+//         fadeIn().then(()=>{
+//             slide()
+//         })
+//     })
+// })
 // $(document).ready(function(){
 //         $(".slide-toggle").click(function(){
 //             $(".box").animate({
